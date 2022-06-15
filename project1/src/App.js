83a0c95d38237cba3eb211f6723ca90d89ccb254
Imanda <asis03ktm@gmail.com>
2022-06-15 21:01:22 +0545

    Updated

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./Components/header"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import Todos from "./Components/Todos"
+import List from "./Components/list"
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      
-    </div>
+    <>
+    <Header title="My Todo List" > </Header>
+    <Footer/>
+    <Todos/>
+    <List/>
+    </>
   );
 }
 
---
+++ project1/src/Components/list.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const list = () => {
+  return (
+    <div>list</div>
+  )
+}
+
+export default list;
